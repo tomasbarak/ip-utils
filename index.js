d99e6d29a6aea7499c540697a7541b61a8ef10ac
tomasbarak <barakcinthia@gmail.com>
2022-05-23 17:44:02 -0300

    Added range generator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const {isValid, isLoopback} = require('./lib/validator');
-const {parseInt32, parseStr, parse} = require('./lib/parser');
-const {strToInt32, int32ToBin, int32ToStr, calculateCIDR, binToInt32} = require('./lib/converter');
+const { isValid, isLoopback } = require('./lib/validator');
+const { parseInt32, parseStr, parse } = require('./lib/parser');
+const { strToInt32, int32ToBin, int32ToStr, calculateCIDR, binToInt32 } = require('./lib/converter');
 
 //Validators
 module.exports.isValid = isValid;
@@ -28,8 +28,8 @@ module.exports.myIP = () => {
         let iface = interfaces[devName];
         for (let i = 0; i < iface.length; i++) {
             let alias = iface[i];
-            if(String(alias.family).toLowerCase() === 'ipv4'){
-                if(!isLoopback(alias.address)){
+            if (String(alias.family).toLowerCase() === 'ipv4') {
+                if (!isLoopback(alias.address)) {
                     return {
                         address: alias.address,
                         netmask: {
@@ -41,5 +41,34 @@ module.exports.myIP = () => {
                 }
             }
         }
+    }
+}
+
+/**
+ * 
+ * @param {String, Number, Array, Object} from IP Address to start generating range
+ * @param {String, Number, Array, Object} to IP Address to end generating range 
+ * @param {Boolean} includeBorders Include borders in range 
+ * @returns Array of generated IP addresses
+ */
+
+module.exports.generateIPRange = (from, to, includeBorders = false) => {
+    if(isValid(from) && isValid(to)){
+        const from_arr = parse(from);
+        const to_arr = parse(to);
+
+        const from_int = strToInt32(from_arr.join('.'));
+        const to_int = strToInt32(to_arr.join('.'));
+
+        let range = [];
+        let offset = includeBorders ? 0 : 1;
+
+        for (let i = from_int + offset; i <= to_int - offset; i++) {
+            range.push(int32ToStr(i));
+        }
+
+        return range;
+    }else{
+        throw new Error('Invalid IP address');
     }
 }
